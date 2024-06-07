9cf119bedd51bf422cac26564cc19bf74be96f94
YashNathani45 <114227625+YashNathani45@users.noreply.github.com>
2024-06-07 18:53:02 +0530

    Update SignupForm.js

--- client/src/components/SignupForm.js
+++ client/src/components/SignupForm.js
@@ -25,7 +25,7 @@ function SignupForm() {
     e.preventDefault();
 
     try {
-      const response = await fetch(`http://localhost:5000/api/opportunities/${opportunityId}/signup`, {
+      const response = await fetch(`https://volunteer-recruitment.vercel.app/api/opportunities/${opportunityId}/signup`, {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
